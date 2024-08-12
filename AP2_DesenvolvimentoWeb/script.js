11b790dd7077ccbda19df7b621eaa29203d5d7a0
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2024-08-12 13:32:53 -0300

    Add files via upload

---
+++ AP2_DesenvolvimentoWeb/script.js
@@ -0,0 +1,55 @@
+window.onload = () => {
+    carregarJogadores()
+}
+
+let elencoFeminino = document.getElementById('elenco-feminino')
+
+let elencoMasculino = document.getElementById('elenco-masculino')
+
+
+
+const carregarJogadores = () => {  
+   jogadores.forEach((jogador, indice)=>{
+        if(jogador["elenco"] == "feminino"){
+            elencoFeminino.innerHTML += `
+
+            <div class="jogador" id="jogador${indice}" onclick="salvarInformacoes(${indice})">
+                <img src="${jogador["imagem"]}">
+                <span class = "nome">${jogador["nome"]}</span>
+            </div>
+    
+            `;
+
+        } else {
+            elencoMasculino.innerHTML += `
+
+            <div class="jogador" id="jogador${indice}" onclick="salvarInformacoes(${indice})">
+              
+                <img src="${jogador["imagem"]}">
+                <span class = "nome">${jogador["nome"]}</span>
+            </div>
+    
+            `;
+        }
+        
+   })
+}
+
+
+
+
+
+function salvarInformacoes(indiceJogador) {
+    const jogador = jogadores[indiceJogador];
+    console.log('falhou')
+    localStorage.setItem('@Nome', jogador.nome)
+    localStorage.setItem('@Imagem', jogador.imagem)
+    localStorage.setItem('@Posicao', jogador.posicao)
+    localStorage.setItem('@Descricao', jogador.descricao)
+    localStorage.setItem('@NomeCompleto', jogador.nome_completo)
+    localStorage.setItem('@Nascimento', jogador.nascimento)
+    localStorage.setItem('@Altura', jogador.altura);
+
+
+    window.location.href = './detalhes.html'
+}
